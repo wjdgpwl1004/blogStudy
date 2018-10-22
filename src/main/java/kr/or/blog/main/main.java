775676a9e13bf0c7394053b33d360fc46474d4d1
June <kimono10@naver.com>
2018-10-22 11:47:54 +0900

    master branch push

--- src/main/java/kr/or/blog/main/main.java
+++ src/main/java/kr/or/blog/main/main.java
@@ -8,8 +8,7 @@ class TestController{
 
     @RequestMapping(value="/")
     public String index(){
-        System.out.println("Test!!");
-        System.out.println("master_branch");
+        System.out.println("master_branch!!!");
         return "index";
     }
 
