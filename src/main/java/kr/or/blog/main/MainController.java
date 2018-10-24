3a4b8822aedeac73d0f65c8571c7d338ab0502a9
June <kimono10@naver.com>
2018-10-24 16:59:29 +0900

    mainController 테스트 로직 삭제

--- src/main/java/kr/or/blog/main/MainController.java
+++ src/main/java/kr/or/blog/main/MainController.java
@@ -10,34 +10,17 @@ import kr.or.blog.constant.Template;
 @Controller
 public class MainController{
 
-    @RequestMapping(path="/index")
-    public String index(){
-        System.out.println("master_branch!!!");
-        return "index";
-    }
-
     @RequestMapping(path="/")
     public String home(Model model){
         model.addAttribute("template",Template.HOME);
         return Template.MAIN;
     }
 
-    @RequestMapping(path="/{userId}")
+    @RequestMapping(path="/{userId}.BStory")
     public String userMain(
         Model model,
         @PathVariable(name="userId") String userId){
-        return "";
-    }
-
-    @RequestMapping(path="/test")
-    public String test(){
-        System.out.println("Test");
-        return "hello";
-    }
-
-    @RequestMapping(path="/freemarker")
-    public String freemarker(){
-        System.out.println("freemarker!!");
-        return "freemarker";
+        model.addAttribute("template",Template.HOME);
+        return Template.MAIN;
     }
 }
