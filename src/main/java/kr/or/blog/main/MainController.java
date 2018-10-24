0a6bba25c5bf27dfae6c2d8acbf4ab5734d635a6
June <kimono10@naver.com>
2018-10-24 16:28:14 +0900

    mainController RequestMapping value -> path 속성으로 변경

--- src/main/java/kr/or/blog/main/MainController.java
+++ src/main/java/kr/or/blog/main/MainController.java
@@ -2,6 +2,7 @@ package kr.or.blog.main;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.blog.constant.Template;
@@ -9,25 +10,32 @@ import kr.or.blog.constant.Template;
 @Controller
 public class MainController{
 
-    @RequestMapping(value="/index")
+    @RequestMapping(path="/index")
     public String index(){
         System.out.println("master_branch!!!");
         return "index";
     }
 
-    @RequestMapping(value="/")
+    @RequestMapping(path="/")
     public String home(Model model){
         model.addAttribute("template",Template.HOME);
         return Template.MAIN;
     }
 
-    @RequestMapping(value="/test")
+    @RequestMapping(path="/{userId}")
+    public String userMain(
+        Model model,
+        @PathVariable(name="userId") String userId){
+        return "";
+    }
+
+    @RequestMapping(path="/test")
     public String test(){
         System.out.println("Test");
         return "hello";
     }
 
-    @RequestMapping(value="/freemarker")
+    @RequestMapping(path="/freemarker")
     public String freemarker(){
         System.out.println("freemarker!!");
         return "freemarker";
