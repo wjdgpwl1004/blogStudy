400ea8815b99d7c3d4ae873ac54344394767e22f
June <kimono10@naver.com>
2018-10-26 09:06:15 +0900

    메인 수정

--- src/main/java/kr/or/blog/blog/BlogApplication.java
+++ src/main/java/kr/or/blog/blog/BlogApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 //import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
