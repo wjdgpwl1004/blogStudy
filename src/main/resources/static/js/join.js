f0dd2cdb26848a4684d46dcf52f6aa1802b29c30
June <kimono10@naver.com>
2018-10-26 14:33:17 +0900

    아이디 중복검사 js  추가

--- src/main/resources/static/js/join.js
+++ src/main/resources/static/js/join.js
@@ -1,7 +1,36 @@
+const MSG = '사용가능한 아이디입니다.';
+function obj(){
+    var idCheck = false;
+    var message = '이미 사용중인 아이디입니다';
+    return {
+        getCheckState(){
+            return idCheck
+        },
+        setCheckState(status){
+            idCheck = status;
+        },
+        getMessage(){
+            return message;
+        },
+        setMessage(msg){
+            message = msg;
+        }
+    }
+};
+var obj = new obj();
+$(function(){
+    $('#id').on('change',function(){
+        obj.setCheckState(false);
+    });
+});
+
 // 회원가입 체크 함수
 function fn_validation(form){
     // 정규식체크
-
+    if(!obj.getCheckState()){
+        alert('아이디 중복검사를 진행해주세요.');
+        return false;
+    }
 
     // 연락처 value값 세팅
     var phone_prefix = form.phone_prefix.value;
@@ -21,11 +50,32 @@ function fn_submit(frm){
     if(frm == null){
         form = document.frm;
     }
-    if(confirm('가입하시겠습니까 ? ') && fn_validation(form)){
-        var id = form.id.value;
-        form.action = `/join/${id}`;
-        form.submit();
+    if(confirm('가입하시겠습니까 ? ')){
+        if(fn_validation(form)){
+            alert('pass');
+            var id = form.id.value;
+            form.action = `/join/${id}`;
+            form.submit();
+        }
     }
-
     return false;
+}
+
+function idCheck(){
+    var id = document.frm.id.value;
+    if(id.length <= 0){
+        alert('아이디를 입력해주세요.');
+        return false;
+    }
+    
+    $.ajax({
+        url : `/join/${id}`,
+        dataType : 'json'
+    }).then(function(res){
+        if(res.result){
+            obj.setCheckState(true);
+            obj.setMessage(MSG);
+        }
+        alert(obj.getMessage());
+    });
 }
